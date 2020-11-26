f80a33d97c096563b7a2444d7055aadd77baadce
MarilenaRoque <marilenas@id.uff.br>
2020-11-26 19:26:17 -0300

    Set game runner template
    Set main options of the game
    Set platform pool functionality
    Set Player main features
    Set jump and double jump
    Add images

--- src/Scenes/PreloaderScene.js
+++ src/Scenes/PreloaderScene.js
@@ -11,7 +11,7 @@ export default class PreloaderScene extends Phaser.Scene {
 
   preload () {
     // add logo image
-    this.add.image(400, 200, 'logo');
+    this.add.image(400, 150, 'logo');
 
     // display progress bar
     var progressBar = this.add.graphics();
