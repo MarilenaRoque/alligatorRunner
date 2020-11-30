4cc7c55cf20457bf52a2d53a0dd8bb81715e5d30
MarilenaRoque <marilenas@id.uff.br>
2020-11-30 20:36:36 -0300

    Add tests to model

---
+++ src/tests/Model.test.js
@@ -0,0 +1,31 @@
+import Model from '../Model';
+let newModel = new Model;
+
+describe('Test Model creation', () => {
+    it('Testing if model returns an object', () => {
+        expect(typeof newModel).toBe('object');
+    });
+    it('Testing if that object have the right attributes', () => {
+        expect(newModel).toHaveProperty('_soundOn');
+    });
+    it('Testing if that object have the right attributes', () => {
+        expect(newModel).toHaveProperty('_musicOn');
+    });
+    it('Testing if that object have the right attributes', () => {
+        expect(newModel).toHaveProperty('_bgMusicPlaying');
+    });
+});
+
+describe('Test if model properties are initialized with righ values', () => {
+
+    it('Testing if that object have the right attributes', () => {
+        expect(newModel._soundOn).toBe(true);
+    });
+    it('Testing if that object have the right attributes', () => {
+        expect(newModel._musicOn).toBe(true);
+    });
+    it('Testing if that object have the right attributes', () => {
+        expect(newModel._bgMusicPlaying).toBe(false);
+    });
+    
+});
--- src/tests/leaderboard.test.js
+++ src/tests/leaderboard.test.js
@@ -1,8 +1,8 @@
 import leaderboard from '../leaderboard';
 
 
-describe('Testing pageInit function', () => {
-    it('Testing if the Object returned is an Array', () => {
+describe('Test add score response', () => {
+    it('Testing fetch is working and respond with the right object', () => {
         const result = leaderboard.addScore('Marilena', 10);
         expect(typeof result).toBe('object');
     })
