49084c98176645a6ba9f644b41c2d143fb95a400
MarilenaRoque <marilenas@id.uff.br>
2020-12-03 17:39:31 -0300

    Set animation to score submission
    - Add image load
    - Set visible/invisible switch for images
    - Built and applied animation

--- src/Scenes/SubmitScore.js
+++ src/Scenes/SubmitScore.js
@@ -13,17 +13,34 @@ export default class SubmitScore extends Phaser.Scene {
   preload() {
     this.load.image('background', 'assets/background.png');
     this.load.image('character', 'assets/over.png');
+     this.load.spritesheet('loadIcon', 'assets/load.png', {
+      frameWidth: 100,
+      frameHeight: 110,
+    });
   }
 
 
   create() {
+  
     let name = '';
+
+    // Creating Loading Animation
+    this.anims.create({
+      key: 'loading',
+      frames: this.anims.generateFrameNumbers('loadIcon', { start: 0, end: 7 }),
+      frameRate: 10,
+      repeat: -1,
+    });
 
 
     const submitInfo = () => {
+      
       const test = document.createElement('h4');
       name = document.getElementById('name').value;
       if (name.length >= 5) {
+        this.character.visible = false;
+        this.loadIcon.visible = true;
+        this.loadIcon.anims.play('loading');
         const result = leaderboard.addScore(name, this.score);
         result.then(() => {
           this.scene.start('Over');
@@ -35,7 +52,9 @@ export default class SubmitScore extends Phaser.Scene {
     };
 
     this.add.image(400, 300, 'background');
-    this.add.image(400, 300, 'character');
+    this.character = this.add.image(400, 300, 'character');
+    this.loadIcon = this.add.sprite(400, 300, 'loadIcon');
+    this.loadIcon.visible = false;
 
     const inputText = document.createElement('input');
     inputText.type = 'text';
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import Phaser from 'phaser';
-import config from './Config/config';
-import GameScene from './Scenes/GameScene';
-import OverScene from './Scenes/OverScene';
-import BootScene from './Scenes/BootScene';
-import PreloaderScene from './Scenes/PreloaderScene';
-import TitleScene from './Scenes/TitleScene';
-import OptionsScene from './Scenes/OptionsScene';
-import CreditsScene from './Scenes/CreditsScene';
-import SubmitScore from './Scenes/SubmitScore';
-import InstructionsScene from './Scenes/Instructions';
-import Model from './Model';
+import Phaser from "phaser";
+import config from "./Config/config";
+import GameScene from "./Scenes/GameScene";
+import OverScene from "./Scenes/OverScene";
+import BootScene from "./Scenes/BootScene";
+import PreloaderScene from "./Scenes/PreloaderScene";
+import TitleScene from "./Scenes/TitleScene";
+import OptionsScene from "./Scenes/OptionsScene";
+import CreditsScene from "./Scenes/CreditsScene";
+import SubmitScore from "./Scenes/SubmitScore";
+import InstructionsScene from "./Scenes/Instructions";
+import Model from "./Model";
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
     const model = new Model();
     this.globals = { model, bgMusic: null };
-    this.scene.add('Boot', BootScene);
-    this.scene.add('Preloader', PreloaderScene);
-    this.scene.add('Title', TitleScene);
-    this.scene.add('Options', OptionsScene);
-    this.scene.add('Credits', CreditsScene);
-    this.scene.add('Submit', SubmitScore);
-    this.scene.add('Game', GameScene);
-    this.scene.add('Instructions', InstructionsScene);
-    this.scene.add('Over', OverScene);
-    this.scene.start('Boot');
+    this.scene.add("Boot", BootScene);
+    this.scene.add("Preloader", PreloaderScene);
+    this.scene.add("Title", TitleScene);
+    this.scene.add("Options", OptionsScene);
+    this.scene.add("Credits", CreditsScene);
+    this.scene.add("Submit", SubmitScore);
+    this.scene.add("Game", GameScene);
+    this.scene.add("Instructions", InstructionsScene);
+    this.scene.add("Over", OverScene);
+    this.scene.start("Submit");
   }
 }
 
